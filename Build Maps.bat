3c9e2b4d1824eebb6595a21ed22b759199da7815
Kevin Scroggins <nitro404@gmail.com>
2010-02-16 00:09:57 +0000

    Removed Original Map Builder and Texture Compiler  - No longer need old Builder, it's obsolete and has been long-replaced  - Merged Texture Compiler script into Map Builder script
    git-svn-id: file:///e/SVN/3d_engine@60 463a7de7-ff88-454d-8a36-9b7cb3bdd095

--- Build Maps.bat
+++ Build Maps.bat
@@ -1,6 +1,25 @@
 @ECHO OFF
 CLS
 
+SET RELEASE=Game
+
+
+CD "Texture Compiler"
+ECHO Compiling Texture Compiler...
+javac src\*.java -d bin
+ECHO Done Compiling!
+ECHO.
+CD bin
+ECHO Compiling Texture List...
+java TextureCompiler -d"../../textures/" -i"../animations.ini" -o"../textures.ini"
+ECHO Done Compiling Texture List!
+CD "../../"
+ECHO.
+ECHO Copying Texture List...
+XCOPY "Texture Compiler\textures.ini" "Builder 2.0" /I /Y
+XCOPY "Texture Compiler\textures.ini" "Data" /I /Y
+
+ECHO.
 CD "Builder 2.0"
 ECHO Compiling Map Builder...
 javac src\*.java -d bin
